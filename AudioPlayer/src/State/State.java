e654ba6e4b49d19eabffd0a0dc8a6f0249bb9e1a
mfsolange <mfsolange@gmail.com>
2015-01-18 22:05:57 -0300

    Revert "Topic 1"
    This reverts commit b5f51e67cb0dcc01047a8559b9ba62acb286dc59.

--- AudioPlayer/src/State/State.java
+++
@@ -1,9 +0,0 @@
-package State;
-import Player.MusicPlayer;
-
-
-public interface State {
-	public void next(MusicPlayer player);
-	public void back(MusicPlayer player);
-	public void show();
-}
--- ShoppingCart/src/Cart/Observer.java
+++
@@ -1,9 +0,0 @@
-package Cart;
-
-import Item.Item;
-
-public interface Observer {
-	public void notifyPriceChange(Item item);
-	public void notifyAddingItem();
-	public void notifyNewTransaction(int ID);
-}
